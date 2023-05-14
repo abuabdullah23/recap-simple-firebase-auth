59f938fd2aedc6e98a967e618fbcf056a8a3e48f
abuabdullah23 <abuobaidazayed@gmail.com>
2023-05-15 04:37:19 +0600

    Basic Route Setup with header, home, login components

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,35 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/Home.jsx';
+import Main from './components/Main.jsx';
+import Login from './components/Login.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/login',
+        element: <Login></Login>
+      }
+    ]
+  },
+]);
+
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
